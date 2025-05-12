450fe41b5b520ea02f939a5c70eb20459a1cce5e
Emith Dinsara <emithdinsara03@gmail.com>
2025-05-12 15:02:14 +0530

    containerize bug fixes

--- client/app/auth/register/page.tsx
+++ client/app/auth/register/page.tsx
@@ -54,7 +54,7 @@ const RegisterPage = () => {
         if (!token) {
           return;
         }
-        const response = await fetch('http://localhost:8080/api/auth/profile', {
+        const response = await fetch(`${process.env.BE_URL}/api/auth/profile`, {
           method: 'GET',
           headers: {
             'Authorization': `Bearer ${token}`,
